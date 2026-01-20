c6f59afa1bf80f4c6b6178542856c3d7718f5230
deven367 <masterdeven@gmail.com>
2026-01-20 13:14:30 -0500

    `toarray`

--- experiments/py/eval_utils_counterfact.py
+++ experiments/py/eval_utils_counterfact.py
@@ -261,6 +261,6 @@ def compute_freq(sentence, n=2):
 
 
 def tfidf_similarity(text_a, text_b, vec):
-    encs = vec.transform([text_a, text_b]).A
+    encs = vec.transform([text_a, text_b]).toarray()
     norm = np.linalg.norm
     return (np.dot(encs[0], encs[1]) / norm(encs[0]) / norm(encs[1])).item()
